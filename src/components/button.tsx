d6df4ed035364df7e012e8894348976fcb0d57be
oliveiradniel <kadadniel@gmail.com>
2026-01-21 18:08:21 -0300

    feat: add button component

---
+++ src/components/button.tsx
@@ -0,0 +1,23 @@
+import type { ComponentProps } from 'react';
+
+import { tv, type VariantProps } from 'tailwind-variants';
+
+export type ButtonProps = ComponentProps<'button'> &
+  VariantProps<typeof button>;
+
+const button = tv({
+  base: 'bg-primary text-over-primary h-10 hover:bg-primary/70 flex  cursor-pointer items-center justify-center rounded-sm transition-colors',
+  variants: {
+    size: {
+      default: 'w-full max-w-[180px]',
+      icon: 'w-10',
+    },
+  },
+  defaultVariants: {
+    size: 'default',
+  },
+});
+
+export function Button({ className, size, ...props }: ButtonProps) {
+  return <button className={button({ size, className })} {...props} />;
+}
