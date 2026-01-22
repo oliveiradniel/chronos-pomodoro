eaf0f8e7280fd06fbbfe4a1f1cadb487f45904df
oliveiradniel <kadadniel@gmail.com>
2026-01-22 17:25:35 -0300

    feat: add header component

---
+++ src/components/header.tsx
@@ -0,0 +1,37 @@
+import {
+  HomeIcon,
+  HistoryIcon,
+  TimerIcon,
+  SettingsIcon,
+  SunIcon,
+} from 'lucide-react';
+
+import { Button } from './button';
+
+export function Header() {
+  return (
+    <header className="flex flex-col gap-8">
+      <h1 className="text-primary flex flex-col items-center gap-2 text-4xl font-bold">
+        <TimerIcon aria-hidden="true" className="size-15" /> Chronos
+      </h1>
+
+      <div className="flex items-center justify-center gap-4">
+        <Button size="icon" type="button">
+          <HomeIcon />
+        </Button>
+
+        <Button size="icon" type="button">
+          <HistoryIcon />
+        </Button>
+
+        <Button size="icon" type="button">
+          <SettingsIcon />
+        </Button>
+
+        <Button size="icon" type="button">
+          <SunIcon />
+        </Button>
+      </div>
+    </header>
+  );
+}
