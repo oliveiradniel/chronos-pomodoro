f10cf8f0450696625f2debc4a3d7f72952ee681d
oliveiradniel <kadadniel@gmail.com>
2026-01-26 15:00:36 -0300

    feat: add cycles component

---
+++ src/components/cycles.tsx
@@ -0,0 +1,18 @@
+export function Cycles() {
+  return (
+    <div className="mt-10 flex items-center gap-2">
+      <span>Ciclos:</span>
+
+      <div className="flex items-center gap-1">
+        <div className="bg-primary size-4.5 rounded-full" />
+        <div className="bg-warning size-4.5 rounded-full" />
+        <div className="bg-primary size-4.5 rounded-full" />
+        <div className="bg-warning size-4.5 rounded-full" />
+        <div className="bg-primary size-4.5 rounded-full" />
+        <div className="bg-warning size-4.5 rounded-full" />
+        <div className="bg-primary size-4.5 rounded-full" />
+        <div className="bg-warning size-4.5 rounded-full" />
+      </div>
+    </div>
+  );
+}
